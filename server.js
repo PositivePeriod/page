0492ee1e045e7e98a797deb5a11c9837044d9a09
PositivePeriod <positiveperiod.dev@gmail.com>
2021-06-07 17:13:40 +0900

    feat: update status code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.set('view-engine', 'ejs');
@@ -12,15 +13,21 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use(cors());
+
 // main page by static
 // app.use('/', express.static(path.join(__dirname, `/views/main`)));
 
 app.get('/develop/Text%20beautify', function(req, res, next) {
-    res.redirect('/beautify');
+    res.set('location', '/beautify');
+    res.status(301).send();
+    // res.redirect(301, '/beautify');
 });
 
 app.get('/game/IdentityV', function(req, res, next) {
-    res.redirect('/identityV');
+    res.set('location', '/identityV');
+    res.status(301).send();
+    // res.redirect(301, '/identityV');
 });
 
 // make index.ejs as the default for url ended with "/"
@@ -31,15 +38,15 @@ app.all(/\/$/, function(req, res, next) {
         res.render(filePath);
     } else { next(); }
 });
-
 app.use('/', express.static(path.join(__dirname, `/views`)));
 
 app.use((req, res, next) => {
     console.log(req.url);
-    res.redirect('/404/');
+    res.status(404).redirect('/404/');
 });
 
 const port = process.env.PORT || 3000;
 server = app.listen(port, function() {
     console.log(`Personal page of Jeuk Hwang listening on port ${port}`);
+    console.log(`http://localhost:${port}`);
 });
